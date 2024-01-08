ec8ab835ebe21475a986a3ef41170cac8ab372b5
Miguel-Paredes <os.miguelparedes@gmail.com>
2024-01-08 16:50:34 -0500

    update

--- src/controllers/veterinario_controller.js
+++ src/controllers/veterinario_controller.js
@@ -1,44 +1,83 @@
+// Importar el modelo
+import Veterinario from "../models/Veterinario.js"
+
+
 // Metodo para el login
 const login =(req,res)=>{
     res.status(200).json({res:'login del veterinario'})
 }
 
-// Metodo para el registros
+// Metodo para mostrar el pefil
 const perfil=(req,res)=>{
     res.status(200).json({res:'perfil del veterinario'})
 }
 
-// Ruta para el confirmar el token
+// Metodo para el registro
+const registro =async (req,res)=>{
+    // Desesctructurar los campos
+    const {email,password} = req.body
+    // Validar todos los campos llenos
+    if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
+    // Obtener el usuario de la BDD en base al email
+    const verificarEmailBDD = await Veterinario.findOne({email})
+    // Validar que el email sea nuevo 
+    if(verificarEmailBDD) return res.status(400).json({msg:"Lo sentimos, el email ya se encuentra registrado"})
+    // Crear la instancia del nuevo veterinario
+    const nuevoVeterinario = new Veterinario(req.body)
+    // Encriptar el password
+    nuevoVeterinario.password = await nuevoVeterinario.encrypPassword(password)
+    // Crear el token => mail
+    nuevoVeterinario.crearToken()
+    // Guardar en la BDD
+    await nuevoVeterinario.save()
+    // Responder
+    res.status(200).json({nuevoVeterinario})
+}
+
+
+// Metodo para el confirmar el token
 const confirmEmail = (req,res)=>{
     res.status(200).json({res:'confirmar email de registro de veterinario'})
 }
 
-// Ruta para listar veterinarios
+// RMetodouta para listar veterinarios
 const listarVeterinarios = (req,res)=>{
     res.status(200).json({res:'lista de veterinarios registrados'})
 }
-const registro =(req,res)=>{
-    res.status(200).json({res:'registro de un nuevo veterinario'})
-}
+
+// Metodo para mostrar el detalle de un veterinario en particular
 const detalleVeterinario = (req,res)=>{
     res.status(200).json({res:'detalle de un eterinario registrado'})
 }
+
+// Metodo para actualizar el perfil
 const actualizarPerfil = (req,res)=>{
     res.status(200).json({res:'actualizar perfil de un veterinario registrado'})
 }
+
+// Metodo para actualizar el password
 const actualizarPassword = (req,res)=>{
     res.status(200).json({res:'actualizar password de un veterinario registrado'})
 }
+
+// Metodo para recuperar el password
 const recuperarPassword= (req,res)=>{
     res.status(200).json({res:'enviar mail recuperación'})
 }
+
+// Metodo para comprobar el token
 const comprobarTokenPasword= (req,res)=>{
     res.status(200).json({res:'verificar token mail'})
 }
+
+// Metodo para recuperar el password
 const nuevoPassword= (req,res)=>{
     res.status(200).json({res:'crear nuevo password'})
 }
 
+
+
+// Exportar cada uno de los metodos
 export {
     login,
     perfil,
--- src/routers/veterinario_routes.js
+++ src/routers/veterinario_routes.js
@@ -4,40 +4,51 @@ import {Router} from 'express'
 // Crear una instancia de Router()
 const router = Router()
 
+import {
+    login,
+    perfil,
+    registro,
+    confirmEmail,
+    listarVeterinarios,
+    detalleVeterinario,
+    actualizarPerfil,
+    actualizarPassword,
+    recuperarPassword,
+    comprobarTokenPasword,
+    nuevoPassword,
+} from "../controllers/veterinario_controller.js";
+
+router.post("/login", login);
+router.post("/registro", registro);
+router.get("/confirmar/:token", confirmEmail);
+router.get("/veterinarios", listarVeterinarios);
+router.get("/recuperar-password", recuperarPassword);
+router.get("/recuperar-password/:token", comprobarTokenPasword);
+router.post("/nuevo-password/:token", nuevoPassword);
+
+router.get("/perfil", perfil);
+router.put('/veterinario/actualizarpassword',actualizarPassword)
+router.get("/veterinario/:id", detalleVeterinario);
+router.put("/veterinario/:id", actualizarPerfil);
+
 // Definir las rutas
-    // Ruta para el login
-    router.post('/login',(req,res)=>res.send("login"))
-    
-    // Ruta para el registro
-    router.post('/registro',(req,res)=>res.send("registro"))
-    
-    // Ruta para el confirmar el token
-    router.get('/confirmar/:token',(req,res)=>res.send("confirmar email"))
-    
-    // Ruta para listar veterinarios
-    router.get('/veterinarios',(req,res)=>res.send("lista de veterinarios"))
-    
-    // Ruta para recuperar el password
-    router.get('/recuperar-password',(req,res)=>res.send("enviar mail"))
-    
-    // Ruta para recuperar el password con el token
-    router.get('/recuperar-password/:token',(req,res)=>res.send("verificar token"))
-    
-    // Ruta para generar el nuevo password
-    router.post('/nuevo-password/:token',(req,res)=>res.send("crear password"))
-    
-    // Ruta para mostrar el perfil del usuario
-    router.get('/perfil',(req,res)=>res.send("perfil"))
-    
-    // Ruta para generar actualizar el password
-    router.put('/veterinario/actualizarpassword',(req,res)=>res.send("actualizar password"))
-    
-    // Ruta para mostrar el detalle de un veterinario en particular
-    router.get('/veterinario/:id',(req,res)=>res.send("detalle del veterinario"))
-    
-    // Ruta para actualizar un veterinario en particular
-    router.put('/veterinario/:id',(req,res)=>res.send("actualizar perfil"))
+router.post("/login", login);
+router.post("/registro", registro);
+router.get("/confirmar/:token", confirmEmail);
+router.get("/veterinarios", listarVeterinarios);
+router.get("/recuperar-password", recuperarPassword);
+router.get("/recuperar-password/:token", comprobarTokenPasword);
+router.post("/nuevo-password/:token", nuevoPassword);
+
+router.get("/perfil", perfil);
+router.put('/veterinario/actualizarpassword',actualizarPassword)
+router.get("/veterinario/:id", detalleVeterinario);
+router.put("/veterinario/:id", actualizarPerfil);
 
 
 // Exportar la variable router
 export default router
+
+
+
+
